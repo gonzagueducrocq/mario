ea934b11c2d8a0474f83ecbaf98dbab23b43205b
Unknown <gonzague.ducrocq@soprasteria.com>
2017-09-06 11:48:42 +0200

    deplacement mario

--- mario.js
+++ mario.js
@@ -1,16 +1,35 @@
 $(document).ready(function() {
+
+var tabPosition = ["", "p1", "p2", "p3"];
+var currentPosition = 0;
+
   $(document).on('keydown', function(e) {
 
 var position = $("#mario").position().left;
+var classSens = "";
 
     if (e.keyCode == 39) {
       position = position + 10;
+      currentPosition = currentPosition + 1;
+
+      if (currentPosition > 3) {
+        currentPosition = 1;
+      }
+
     }
     else if (e.keyCode == 37) {
+      classSens = "reverse";
       position = position - 10;
+      currentPosition = currentPosition +1;
+
+      if (currentPosition > 3) {
+        currentPosition = 1;
+      }
+
     }
     if (position <0 ) {
       position = 0;
+      currentPosition = 0;
     }
 
     var tailleMax = $("#plateau").width() - $("#mario").width();
@@ -20,6 +39,8 @@ var position = $("#mario").position().left;
     }
 
 $("#mario").css('left', position +"px");
+$("#mario").attr('class', classSens + " " +tabPosition[currentPosition]);
+
 
 //var position = $("#mario").position().top;
 //  if (e.keyCode == 38) {
